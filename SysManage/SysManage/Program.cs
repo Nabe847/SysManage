7922e4abdbde50258790d184446e41a47eb15e7a
manabe-t <manabetakumi02@gmail.com>
2019-10-26 20:43:52 +0900

    add initial commit

---
+++ SysManage/SysManage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SysManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
